895116f7011a8fbc4ce7819a583d2cf0b43a2e62
Nikita Meena <nikita.jonwal@thoughtwin.com>
2024-04-22 17:49:54 +0530

    create:postgres database

--- My_project/settings.py
+++ My_project/settings.py
@@ -28,14 +28,20 @@ SECRET_KEY = 'django-insecure-=ysi&6w6typ#ly_i4gv#x1-8%(wqvnl($8(71sgleckxq(fig&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "nikita.jonwal@thoughtwin.com"
-EMAIL_HOST_PASSWORD = "rxyj brac ntnw zbki"
-EMAIL_PORT=587
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL=False
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+import os
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Email configuration
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS") == "True"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL") == "True"
+EMAIL_BACKEND = os.getenv("EMAIL_BACKEND")
 ALLOWED_HOSTS = []
 
 
@@ -87,10 +93,21 @@ WSGI_APPLICATION = 'My_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.sqlite3',
+#     #     'NAME': BASE_DIR / 'db.sqlite3',
+#     # }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 
--- my_app/views.py
+++ my_app/views.py
@@ -9,48 +9,6 @@ from django.core.mail import send_mail
 
 
 
-# def register(request):
-#     # breakpoint()
-#     if request.method == 'POST':
-#         # breakpoint()
-#         first_name = request.POST.get('first_name')
-#         last_name = request.POST.get('last_name')
-#         email = request.POST.get('email_id')
-#         phone_number = request.POST.get('phone_number')
-#         password = request.POST.get('password')
-#         confirm_password = request.POST.get('confirm_password')
-        
-#         if not first_name or not last_name or not email or not phone_number or not password or not confirm_password:
-#             messages.error(request, "All fields are required.")
-#             return redirect('register')
-        
-    
-#         if Student.objects.filter(email_id=email).exists():
-#             messages.error(request, "Email is already registered.")
-#             return redirect('register')
-        
-#         if password != confirm_password:
-#             messages.error(request, "Passwords do not match.")
-#             return redirect('register')
-        
-#         student = Student.objects.create(
-#             first_name=first_name,
-#             last_name=last_name,
-#             email_id=email,
-#             phone_number=phone_number,
-#             password=password, 
-#             confirm_password=confirm_password
-#         )
-#         student.save()
-#         subject ="Welcome To My User Register From"
-#         messages=f"Hello..{student.first_name} \n\nThank you for registering on our website. Please confirm your email address to activate your account.\n\nRegards,\nThe Django Team"
-#         form_email=settings.EMAIL_HOST_USER
-#         recipient_list=[student.email_id,]
-#         send_mail(subject,messages,form_email,recipient_list)
-#         return redirect('home')
-    
-#     return render(request, 'register.html')
-
 def register(request):
     # breakpoint()
     if request.method == 'POST':
